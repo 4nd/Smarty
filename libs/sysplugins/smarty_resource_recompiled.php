9783b69d7dbe2cdadf18217b7b0c06e66ef9afe2
andy <andy@31interactive.com>
2014-04-01 18:36:20 +0100

    Initial commit

---
+++ libs/sysplugins/smarty_resource_recompiled.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Smarty Resource Plugin
+ *
+ *
+ * @package TemplateResources
+ * @author Rodney Rehm
+ */
+
+/**
+ * Smarty Resource Plugin
+ *
+ * Base implementation for resource plugins that don't compile cache
+ *
+ *
+ * @package TemplateResources
+ */
+abstract class Smarty_Resource_Recompiled extends Smarty_Resource
+{
+}
